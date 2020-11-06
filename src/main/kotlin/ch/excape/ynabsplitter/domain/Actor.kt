3ded8f864ee757a96151fc45d6ee4419a4b9e616
Suter, Robin <robin.suter@zuehlke.com>
2020-11-06 20:20:39 +0100

    adjust ids for new budget

--- src/main/kotlin/ch/excape/ynabsplitter/domain/Actor.kt
+++ src/main/kotlin/ch/excape/ynabsplitter/domain/Actor.kt
@@ -2,7 +2,7 @@ package ch.excape.ynabsplitter.domain
 
 enum class Actor(val firstName: String, val budgetId: String, val accountId: String) {
     ROBIN("Robin","fef04036-5381-46ce-978d-375507a9e26e", "d4a3b145-e1c7-4ccf-914e-b80d16bb82f6"),
-    SOPHIE("Sophie","12586d8c-2d38-4abd-be08-fb807a84f359", "13e8c014-3c9e-4d59-acb6-fb90c59a0296");
+    SOPHIE("Sophie","710d9d09-a1e8-4951-84cc-a5c261947839", "9bd454eb-fd28-4472-a629-11df1c0d2272");
 
     override fun toString(): String {
         return firstName
