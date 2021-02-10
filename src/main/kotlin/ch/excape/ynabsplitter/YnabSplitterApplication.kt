8357270008eb21c8de1014b02d0a1c81acc0f5bd
Suter, Robin <robin.suter@zuehlke.com>
2021-02-10 08:57:12 +0100

    disable scheduler, set debug logging for troubleshooting

--- src/main/kotlin/ch/excape/ynabsplitter/YnabSplitterApplication.kt
+++ src/main/kotlin/ch/excape/ynabsplitter/YnabSplitterApplication.kt
@@ -4,14 +4,13 @@ import ch.excape.ynabsplitter.rest.YnabSplitterProperties
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.boot.runApplication
-import org.springframework.scheduling.annotation.EnableScheduling
 
 
 @SpringBootApplication
 @EnableConfigurationProperties(YnabSplitterProperties::class)
-@EnableScheduling
+//@EnableScheduling
 class YnabSplitterApplication
 
 fun main(args: Array<String>) {
-	runApplication<YnabSplitterApplication>(*args)
+    runApplication<YnabSplitterApplication>(*args)
 }
