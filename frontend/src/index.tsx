cb4b2dd0eed953c44311fdfb5614bd62be1663db
Suter, Robin <robin.suter@zuehlke.com>
2021-02-15 08:37:34 +0100

    disable service workers to debug on iOS

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -4,9 +4,9 @@ import './index.css';
 import * as serviceWorker from './ServiceWorkerRegistration';
 import Root from './Root';
 
-ReactDOM.render(<Root />, document.getElementById('root'));
+ReactDOM.render(<Root/>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.register();
+serviceWorker.unregister();
